26da9c567d124d049e73cdf684dd96b720a282ea
kirill.koldrysh <kirill.koldrysh@redbit.cz>
2022-08-01 22:16:26 +0200

    Use remote DB for getting data
    Install dotenv library
    Modify get all notes and get note by id endpoints

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
+require('dotenv').config();
 const { response } = require('express');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Note = mongoose.model('Note', noteSchema);
 
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.static('build'));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  const id = request.params.id;
+  Note.findOne({_id: id}).then(note => {
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
+  })
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -103,7 +104,7 @@ const generateId = () => {
   return maxId + 1;
 };
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
